925f0ff45edbec706e1020da5e87817d34b1366a
Teodor Moroz <teodor.moroz@openvpn.net>
2023-05-03 15:19:27 +0300

    Add CloudConnexa bucket type

--- wodles/aws/buckets_s3/__init__.py
+++ wodles/aws/buckets_s3/__init__.py
@@ -1,5 +1,6 @@
 from buckets_s3 import aws_bucket
 from buckets_s3 import cloudtrail
+from buckets_s3 import cloud_connexa
 from buckets_s3 import config
 from buckets_s3 import guardduty
 from buckets_s3 import load_balancers
@@ -11,6 +12,7 @@ from buckets_s3 import waf
 __all__ = [
     "aws_bucket",
     "cloudtrail",
+    'cloud_connexa'
     "config",
     "guardduty",
     "load_balancers",
